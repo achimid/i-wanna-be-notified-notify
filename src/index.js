3f47edb117f913bc191ac6e125d568dafa60bfb1
Lourran Zorzi <lourran-zorzi@lourran.zorzi>
2020-07-25 22:46:40 -0300

    initial commit, criando o projeto

---
+++ src/index.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+
+const cors = require('cors')
+const express = require('express')
+
+const databaseInit = require('./config/database')
+const healthcheck = require('./config/healthcheck')
+
+const app = express()
+
+
+app.use(cors())
+app.use(express.json())
+app.disable('x-powered-by')
+app.use('/api/v1', healthcheck)
+
+// Initializations
+databaseInit()
+
+
+app.listen(process.env.PORT)
+
+
